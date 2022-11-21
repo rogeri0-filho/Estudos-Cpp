d5fc3f69e1eada2ca543398c0936ca8fbf85cc6f
Rogério <rogeriofilho63@gmail.com>
2022-11-21 18:10:26 -0300

    Entrada de Dados em C++

---
+++ Variáveis_e_Entradas/Entradas_de_Dados/Entradas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int idade;
+    cout << "Digite sua idade: " << endl;
+    cin >> idade;
+    cout << "Voce tem : " << idade << " anos" << endl;
+
+    return 0;
+}
