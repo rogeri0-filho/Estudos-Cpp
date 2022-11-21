9d559cfe78e809116ae3b1ee6898d956b35dfdd1
Rogério <rogeriofilho63@gmail.com>
2022-11-21 18:07:45 -0300

    Tipos de dados em C++

---
+++ Variáveis_e_Entradas/Variáveis/Dados.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int Var_Inteiro = 5;
+    cout << Var_Inteiro << endl;
+
+    float Var_Flutuante = 5.5;
+    cout << Var_Flutuante << endl;
+    
+    double Var_Flutuante2 = 5.5e3;
+    cout << Var_Flutuante2 << endl;
+
+    bool Var_Booleana = true;
+    cout << Var_Booleana << endl;
+
+    char Var_Letra_Unica;
+    Var_Letra_Unica = 'a';
+    cout << Var_Letra_Unica << endl;
+
+    string Var_Frase;
+    Var_Frase = "Hello World";
+    cout << Var_Frase << endl;
+
+    return 0;
+}
+
